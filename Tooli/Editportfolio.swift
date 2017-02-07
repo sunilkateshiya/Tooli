ebf466cf6ca5c545f888c38fad81bdf7e89c634e
Impero IT <imperoit@Imperos-iMac.local>
2017-02-07 20:27:04 +0530

    Portifolio

---
+++ Tooli/Editportfolio.swift
@@ -0,0 +1,35 @@
+//
+//  Editportfolio.swift
+//  Tooli
+//
+//  Created by Impero IT on 7/02/2017.
+//  Copyright © 2017 Moin Shirazi. All rights reserved.
+//
+
+import UIKit
+
+class Editportfolio: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
