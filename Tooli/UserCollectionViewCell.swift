fa44c44e4ccc068265a325eab1238db81f7ea22d
Gaurav <gaurav.imperoit@gmail.com>
2017-06-29 12:36:47 +0530

    26-06-2017-1

---
+++ Tooli/UserCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  UserCollectionViewCell.swift
+//  Tooli
+//
+//  Created by Impero IT on 18/05/2017.
+//  Copyright © 2017 Moin Shirazi. All rights reserved.
+//
+
+import UIKit
+
+class UserCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var lblUserName: UILabel!
+    @IBOutlet weak var lblTredaCatagory: UILabel!
+    
+    @IBOutlet weak var btnRemove: UIButton!
+    @IBOutlet weak var btnFollow: UIButton!
+    @IBOutlet weak var imgUserProfile: UIImageView!
+    
+    @IBOutlet weak var btnTaped: UIButton!
+}
---
+++ Tooli/UserlistInDashBorad.swift
@@ -0,0 +1,34 @@
+//
+//  UserlistInDashBorad.swift
+//  Tooli
+//
+//  Created by Impero IT on 18/05/2017.
+//  Copyright © 2017 Moin Shirazi. All rights reserved.
+//
+
+import UIKit
+
+class UserlistInDashBorad: UITableViewCell,UICollectionViewDelegate,UICollectionViewDataSource {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       // collection.register(UserCollectionViewCell.self, forCellWithReuseIdentifier: "UserList")
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       // let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UserList", for: indexPath) as! UserCollectionViewCell
+       // return cell
+        
+        return UICollectionViewCell()
+    }
+}
